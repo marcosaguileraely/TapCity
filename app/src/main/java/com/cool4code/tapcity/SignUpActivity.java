2b33a68f9f436f65baf03665cf0445af34c086d0
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2014-12-22 03:27:25 -0500

    ui user

--- app/src/main/java/com/cool4code/tapcity/SignUpActivity.java
+++ app/src/main/java/com/cool4code/tapcity/SignUpActivity.java
@@ -1,12 +1,17 @@
 package com.cool4code.tapcity;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.ParseException;
@@ -29,8 +34,18 @@ public class SignUpActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        //ParseUtil parseUtil = new ParseUtil();
-        //parseUtil.onCreate();
+        ActionBar bar = getActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFAA00")));
+        int titleId;
+        int textColor = getResources().getColor(android.R.color.white);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            titleId = getResources().getIdentifier("action_bar_title", "id", "android");
+            TextView abTitle = (TextView) findViewById(titleId);
+            abTitle.setTextColor(textColor);
+        } else {
+            TextView abTitle = (TextView) getWindow().findViewById(android.R.id.title);
+            abTitle.setTextColor(textColor);
+        }
 
         name     = (EditText) findViewById(R.id.name);
         lastname = (EditText) findViewById(R.id.lastname);
